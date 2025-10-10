04952df3873e630cf38830739e9c782c4c307ca8
lenrek88 <lenrek88@yandex.ru>
2025-10-10 17:07:54 +0300

    test table finish, add normalize text

--- Parser/parser/parser.go
+++ Parser/parser/parser.go
@@ -2,15 +2,21 @@ package parser
 
 import (
 	"io"
+	"strings"
+	"unicode"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type Movie struct {
-	Title string
-	Time  string
-	Hall  string
-	Price string
+	Title  string
+	Time   string
+	Hall   string
+	Genre  string
+	Price  string
+	Age    string
+	URL    string
+	Format string
 }
 
 func ParseMovies(r io.Reader) ([]Movie, error) {
@@ -18,15 +24,45 @@ func ParseMovies(r io.Reader) ([]Movie, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	movies := make([]Movie, 0)
-	doc.Find(".movie-card").Each(func(i int, card *goquery.Selection) {
+
+	doc.Find(".session").Each(func(i int, card *goquery.Selection) {
+		url, _ := card.Find("a").Attr("href")
 		movie := Movie{
-			Title: card.Find(".title").Text(),
-			Time:  card.Find(".time").Text(),
-			Hall:  card.Find(".hall").Text(),
-			Price: card.Find(".price").Text(),
+			Title:  NormalizeText(card.Find(".title").Text()),
+			Genre:  card.Find(".genre").Text(),
+			Hall:   card.Find(".labels span").Text(),
+			Time:   card.Find(".time").Text(),
+			Age:    card.Find(".age").Text(),
+			Format: card.Find(".format").Text(),
+			Price:  card.Find(".place-price .item").Text(),
+			URL:    url,
 		}
 		movies = append(movies, movie)
 	})
 	return movies, nil
+}
+
+func NormalizeText(text string) string {
+	if len(text) == 0 {
+		return text
+	}
+
+	textFields := strings.Fields(text)
+	for i, word := range textFields {
+		runes := []rune(word)
+		for j := 0; j < len(runes); j++ {
+			if j != 0 {
+				runes[j] = unicode.ToLower(runes[j])
+			}
+		}
+		textFields[i] = string(runes)
+	}
+
+	text = strings.Join(textFields, " ")
+
+	text = strings.Join(strings.Fields(text), " ")
+	text = strings.ReplaceAll(text, "&bnsp", "")
+	return text
 }
--- Parser/parser/parser_test.go
+++ Parser/parser/parser_test.go
@@ -7,62 +7,204 @@ import (
 	"testing"
 )
 
-func TestParseOneMovies(t *testing.T) {
+func TestParseMovies_Table(t *testing.T) {
+	tests := []struct {
+		name, html string
+		want       []Movie
+	}{
+		{"parse any movies",
+			`
+				<div class="session slick-slide slick-active">
+				<a href="/ticket/236100e0-456b-45b5-a719-6db9e44ffcc9/">
+					<div class="title">Ужастики. Ожившие рисунки </div>
+					<div class="head">
+					<div class="photo">
+						<img src="https://cdn.mirage.ru/images/film/7000/small/p7003.jpg" alt="">
+					</div>
+					<div class="content">
+						<div class="genre">комедия, фэнтези, семейный</div>
+						<div class="labels"><span class="blue">Зал №2</span></div>
+						<div class="bot">
+						<div class="info-line">
+							<div class="time">13:10</div>
+							<div class="border">
+							<div class="age">12+</div>
+							<div class="format">2D</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+					<div class="place-price">
+					<div class="item">500₽</div>
+					</div>
+				</a>
+				</div>
+				<div class="session slick-slide slick-active">
+				<a href="/ticket/236100e0-456b-45b5-a719-6db9e44ffcc8/">
+					<div class="title">Комедия. Бегущие за счастьем </div>
+					<div class="head">
+					<div class="photo">
+						<img src="https://cdn.mirage.ru/images/film/7000/small/p7004.jpg" alt="">
+					</div>
+					<div class="content">
+						<div class="genre">комедия, мелодрама, фантастика</div>
+						<div class="labels"><span class="blue">Зал №3</span></div>
+						<div class="bot">
+						<div class="info-line">
+							<div class="time">18:25</div>
+							<div class="border">
+							<div class="age">6+</div>
+							<div class="format">3D</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+					<div class="place-price">
+					<div class="item">550₽</div>
+					</div>
+				</a>
+				</div>
+				`,
 
-	html := `
-    <div class="movie-card">
-      <h3 class="title">Фильм А</h3>
-      <span class="time">18:30</span>
-      <span class="hall">Зал 2</span>
-      <span class="price">350 ₽</span>
-    </div>`
-
-	movies, err := ParseMovies(strings.NewReader(html))
-	if err != nil {
-		t.Fatal(err)
+			[]Movie{{
+				Title:  "Ужастики. Ожившие рисунки",
+				Time:   "13:10",
+				Hall:   "Зал №2",
+				Genre:  "комедия, фэнтези, семейный",
+				Price:  "500₽",
+				Age:    "12+",
+				URL:    "/ticket/236100e0-456b-45b5-a719-6db9e44ffcc9/",
+				Format: "2D",
+			}, {
+				Title:  "Комедия. Бегущие за счастьем",
+				Time:   "18:25",
+				Hall:   "Зал №3",
+				Genre:  "комедия, мелодрама, фантастика",
+				Price:  "550₽",
+				Age:    "6+",
+				URL:    "/ticket/236100e0-456b-45b5-a719-6db9e44ffcc8/",
+				Format: "3D",
+			}},
+		}, {"parse one movies",
+			`
+				<div class="session slick-slide slick-active">
+				<a href="/ticket/236100e0-456b-45b5-a719-6db9e44ffcc9/">
+					<div class="title">Ужастики. Ожившие рисунки </div>
+					<div class="head">
+					<div class="photo">
+						<img src="https://cdn.mirage.ru/images/film/7000/small/p7003.jpg" alt="">
+					</div>
+					<div class="content">
+						<div class="genre">комедия, фэнтези, семейный</div>
+						<div class="labels"><span class="blue">Зал №2</span></div>
+						<div class="bot">
+						<div class="info-line">
+							<div class="time">13:10</div>
+							<div class="border">
+							<div class="age">12+</div>
+							<div class="format">2D</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+					<div class="place-price">
+					<div class="item">500₽</div>
+					</div>
+				</a>
+				</div>
+				`,
+			[]Movie{{
+				Title:  "Ужастики. Ожившие рисунки",
+				Time:   "13:10",
+				Hall:   "Зал №2",
+				Genre:  "комедия, фэнтези, семейный",
+				Price:  "500₽",
+				Age:    "12+",
+				URL:    "/ticket/236100e0-456b-45b5-a719-6db9e44ffcc9/",
+				Format: "2D",
+			}},
+		},
+		{"empty data",
+			" ",
+			[]Movie{},
+		},
+		{"incomplete date",
+			`
+				<div class="session slick-slide slick-active">
+				<a href="/ticket/236100e0-456b-45b5-a719-6db9e44ffcc9/">
+					<div class="title">Ужастики. Ожившие рисунки </div>
+					<div class="head">
+					<div class="photo">
+						<img src="https://cdn.mirage.ru/images/film/7000/small/p7003.jpg" alt="">
+					</div>
+					<div class="content">
+						<div class="genre">комедия, фэнтези, семейный</div>
+						<div class="labels"><span class="blue">Зал №2</span></div>
+						<div class="bot">
+						<div class="info-line">
+							<div class="time"></div>
+							<div class="border">
+							<div class="age">12+</div>
+							<div class="format">2D</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+					<div class="place-price">
+					<div class="item">500₽</div>
+					</div>
+				</a>
+				</div>
+				`,
+			[]Movie{{
+				Title:  "Ужастики. Ожившие рисунки",
+				Time:   "",
+				Hall:   "Зал №2",
+				Genre:  "комедия, фэнтези, семейный",
+				Price:  "500₽",
+				Age:    "12+",
+				URL:    "/ticket/236100e0-456b-45b5-a719-6db9e44ffcc9/",
+				Format: "2D",
+			}},
+		},
 	}
-	m := []Movie{{Title: "Фильм А", Time: "18:30", Hall: "Зал 2", Price: "350 ₽"}}
-	got, _ := json.Marshal(movies)
-	want, _ := json.Marshal(m)
 
-	if !bytes.Equal(got, want) {
-		t.Fatalf("json mismatch:\n got=%s\nwant=%s", got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			movies, _ := ParseMovies(strings.NewReader(tt.html))
+			got, _ := json.Marshal(movies)
+			want, _ := json.Marshal(tt.want)
+			if !bytes.Equal(got, want) {
+				t.Fatalf("json mismatch:\n got=%s\nwant=%s", got, want)
+			}
+		})
 	}
+
 }
 
-func TestParseAnyMovies(t *testing.T) {
-
-	html := `
-    <div class="movie-card">
-      <h3 class="title">Фильм А</h3>
-      <span class="time">18:30</span>
-      <span class="hall">Зал 2</span>
-      <span class="price">350 ₽</span>
-    </div>
-	<div class="movie-card">
-      <h3 class="title">Фильм Б</h3>
-      <span class="time">21:30</span>
-      <span class="hall">Зал 3</span>
-      <span class="price">420 ₽</span>
-    </div>
-	<div class="movie-card">
-      <h3 class="title">Фильм В</h3>
-      <span class="time">09:30</span>
-      <span class="hall">Зал 1</span>
-      <span class="price">550 ₽</span>
-    </div>`
-
-	movies, err := ParseMovies(strings.NewReader(html))
-	if err != nil {
-		t.Fatal(err)
+func TestNormalizeText_Table(t *testing.T) {
+	tests := []struct {
+		name, text, want string
+	}{
+		{"trim multiple spaces", "hi,  people", "hi, people"},
+		{"normalize case", "HeLLo, WoRld hIi WoRld", "Hello, World hii World"},
+		{"html entities", "ok&bnsp", "ok"},
+		{"removing line breaks", "hi, people\n\t\r\n", "hi, people"},
+		{"trim spaces start and end", " hi, people ", "hi, people"},
 	}
-	m := []Movie{{Title: "Фильм А", Time: "18:30", Hall: "Зал 2", Price: "350 ₽"},
-		{Title: "Фильм Б", Time: "21:30", Hall: "Зал 3", Price: "420 ₽"},
-		{Title: "Фильм В", Time: "09:30", Hall: "Зал 1", Price: "550 ₽"}}
-	got, _ := json.Marshal(movies)
-	want, _ := json.Marshal(m)
 
-	if !bytes.Equal(got, want) {
-		t.Fatalf("json mismatch:\n got=%s\nwant=%s", got, want)
+	for _, tt := range tests {
+		ttt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := NormalizeText(tt.text)
+			if got != ttt.want {
+				t.Fatalf("got %q want %q", got, tt.want)
+			}
+		})
 	}
 }
